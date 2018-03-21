9ce6634ace5063f361580659bd243e90b9a37936
Todd Keech <tkeech1@hotmail.com>
2018-03-21 12:09:51 -0400

    first commit

---
+++ lambda_helper.py
@@ -0,0 +1,26 @@
+import json
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': json.dumps({ 'message': err}) if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*',
+        },
+    }
+
+def respond_token(token):
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"token" : {"authenticationToken": token}}),
+        'headers': {'Content-Type': 'application/json',"Access-Control-Allow-Origin":"*"}
+    } 
+    
+def redirect(location):
+    return {
+        "statusCode": 302,
+        "headers": {
+            "Location": location
+        }
+    }     
---
+++ tests/test_helper.py
@@ -0,0 +1,88 @@
+import unittest
+import json
+import lambda_helper
+
+class lambda_helper_test(unittest.TestCase):
+    
+    def test_response_200_json(self): 
+        testVal = {'test':'resp'}
+        self.assertEqual(
+            {
+                'statusCode': '200',
+                'body': json.dumps(testVal),
+                'headers': {'Content-Type': 'application/json','Access-Control-Allow-Origin':'*',},
+            }
+            ,lambda_helper.respond(None,testVal))
+
+    def test_response_200_string(self): 
+        testVal = 'test'
+        self.assertEqual(
+            {
+                'statusCode': '200',
+                'body': json.dumps(testVal),
+                'headers': {'Content-Type': 'application/json','Access-Control-Allow-Origin':'*',},
+            }
+            ,lambda_helper.respond(None,testVal)
+        )
+
+    def test_response_400(self):    
+        testVal = 'An error'
+        self.assertEqual(
+            {
+                'statusCode': '400',
+                'body': json.dumps({ 'message' : testVal}),
+                'headers': {'Content-Type': 'application/json','Access-Control-Allow-Origin':'*',},
+            }
+            ,lambda_helper.respond(testVal,None))
+
+    def test_response_both_values(self):    
+        testVal = 'An error'
+        self.assertEqual(
+            {
+                'statusCode': '400',
+                'body': json.dumps({ 'message' : testVal}),
+                'headers': {'Content-Type': 'application/json','Access-Control-Allow-Origin':'*',},
+            }
+            ,lambda_helper.respond(testVal,testVal))    
+
+    def test_response_only_error_value(self):    
+        testVal = 'An error'
+        self.assertEqual(
+            {
+                'statusCode': '400',
+                'body': json.dumps({ 'message' : testVal}),
+                'headers': {'Content-Type': 'application/json','Access-Control-Allow-Origin':'*',},
+            }
+            ,lambda_helper.respond(testVal))               
+
+    def test_respond_token(self):    
+        testVal = 'test'
+        self.assertEqual( 
+            {
+            'statusCode': 200,
+            'body': json.dumps({'token' : {'authenticationToken': testVal}}),
+            'headers': {'Content-Type': 'application/json',"Access-Control-Allow-Origin":"*"}
+            }
+        , lambda_helper.respond_token(testVal))    
+        self.assertEqual( 
+            {
+            'statusCode': 200,
+            'body': json.dumps({'token' : {'authenticationToken': None}}),
+            'headers': {'Content-Type': 'application/json',"Access-Control-Allow-Origin":"*"}
+            }
+        , lambda_helper.respond_token(None)) 
+
+    def test_redirect(self):    
+        testVal = 'test'
+        self.assertEqual( 
+            {
+            'statusCode': 302,
+            'headers': {"Location": testVal},
+            }
+        , lambda_helper.redirect(testVal))   
+        self.assertEqual( 
+            {
+            'statusCode': 302,
+            'headers': {"Location": None},
+            }
+        , lambda_helper.redirect(None))
